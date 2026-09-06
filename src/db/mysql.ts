1cd95e8e4969efa55e80122b080c043c749b879f
tejesh-kumar <tejeshmuralikrishna@gmail.com>
2026-09-06 21:50:23 +0530

    configure mysql server session timezone is stored in UTC so that it can be adjusted to IST at service layer

--- src/db/mysql.ts
+++ src/db/mysql.ts
@@ -5,11 +5,13 @@ const access: PoolOptions = {
   user: process.env.DB_USER || '',
   password: process.env.DB_PASSWORD || '',
   database: process.env.DB_NAME || '',
+  timezone: 'Z',
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
 };
 
 const db = mysql.createPool(access);
+(async () => await db.query("SET time_zone = '+00:00'"))();
 
 export default db;
